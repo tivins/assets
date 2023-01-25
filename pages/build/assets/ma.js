5b7f42cb409fda3c98b8df2101ea987534ba1cf2
tivins <pub.git@elabo.fr>
2023-01-25 22:52:36 +0100

    update

--- pages/build/assets/ma.js
+++ pages/build/assets/ma.js
@@ -128,6 +128,23 @@ export class Clipboard {
             (err) => callback ? callback(err) : null
         );
     }
+    static parseCopiable() {
+
+        document.querySelectorAll('.copy:not(.copy-processed)').forEach((elm) => {
+            elm.classList.add('copy-processed');
+            elm.addEventListener('click', ev => {
+                ev.preventDefault();
+                const out = elm.querySelector('.copy-out');
+                Clipboard.copyToClipboard(
+                    MA.getTargetText(elm),
+                    (err) => {
+                        const text = elm.hasAttribute('data-text') ? elm.getAttribute('data-text') : 'Copied!';
+                        LocalNotifier.show(elm, err ? err : text, 2000, "fa-regular fa-clone");
+                    }
+                );
+            })
+        });
+    }
 }
 export class MagiCron {
     static #callbacks = [
--- pages/build/assets/main.js
+++ pages/build/assets/main.js
@@ -1,28 +1,11 @@
 import {Clipboard, LocalNotifier, MA, MagiCron} from "/assets/ma.js";
 
 console.debug(MA.version());
-/*
-MagiCron.append(() => {
-    document.querySelectorAll('[data-expire]').forEach(elm => {
-        const expire = elm.getAttribute('data-expire');
-        if (new Date().getTime() > expire) {
-            elm.classList.add('hidden');
-        }
-    });
-}).run();*/
-MagiCron.setInterval(100).run();
 
-document.querySelectorAll('.copy:not(.copy-processed)').forEach((elm) => {
-    elm.classList.add('copy-processed');
-    elm.addEventListener('click', ev => {
-        ev.preventDefault();
-        const out = elm.querySelector('.copy-out');
-        Clipboard.copyToClipboard(
-            MA.getTargetText(elm),
-            (err) => {
-                const text = elm.hasAttribute('data-text') ? elm.getAttribute('data-text') : 'Copied!';
-                LocalNotifier.show(elm, err ? err : text, 2000, "fa-regular fa-clone");
-            }
-        );
-    })
-});
+MagiCron.setInterval(100).run();
+Clipboard.parseCopiable();
+
+export function getLoader()
+{
+    return `<div class="subtext"><i class="fa-solid fa-circle-notch fa-spin mr-2"></i>Loading&hellip;</div>`;
+}
